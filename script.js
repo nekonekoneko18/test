1375521edf582a469a27e6330ea034014e31e607
nekonekoneko18 <kyusakusaku1935@gmail.com>
2026-07-08 20:34:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const button = document.getElementById("searchButton");
 
 button.addEventListener("click", searchBooks);
 
+const APP_ID = "oduZ40wWwLsVSEAlJigX";
+
 async function searchBooks() {
 
     const keyword = document.getElementById("keyword").value;
@@ -12,9 +14,10 @@ async function searchBooks() {
     }
 
     const url =
-        "https://ci.nii.ac.jp/books/opensearch/search?q="
-        + encodeURIComponent(keyword)
-        + "&format=json";
+        "https://ci.nii.ac.jp/books/opensearch/search?"
+        + "q=" + encodeURIComponent(keyword)
+        + "&format=json"
+        + "&appid=" + APP_ID;
 
     console.log(url);
 
@@ -27,6 +30,12 @@ async function searchBooks() {
         const data = await response.json();
 
         console.log(data);
+
+        // 検索結果を画面に表示
+        const results = document.getElementById("results");
+
+        results.innerHTML =
+            "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
 
         alert("取得成功！");
 
