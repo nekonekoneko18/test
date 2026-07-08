72bdca937df77805ec746a7f11012878928e6685
nekonekoneko18 <kyusakusaku1935@gmail.com>
2026-07-08 21:07:09 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -24,26 +24,70 @@ async function searchBooks() {
     try {
 
         const response = await fetch(url);
-
-        console.log(response);
-
         const data = await response.json();
 
         console.log(data);
 
-        // 検索結果を画面に表示
+        // 検索結果表示エリア
         const results = document.getElementById("results");
+        results.innerHTML = "";
 
-        results.innerHTML =
-            "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+        // 検索結果一覧
+        const books = data["@graph"][0]["items"];
 
-       alert(JSON.stringify(data));
+books.forEach(book => {
 
+    // ISBN取得
+    let isbn = "";
+
+    if (book["dc:identifier"]) {
+
+        if (Array.isArray(book["dc:identifier"])) {
+
+            for (const id of book["dc:identifier"]) {
+
+                if (id.startsWith("ISBN")) {
+
+                    isbn = id.replace("ISBN ", "").replace("-", "");
+                    break;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    const imageUrl = isbn
+        ? `https://ndlsearch.ndl.go.jp/thumbnail/${isbn}.jpg`
+        : "images/noimage.png";
+
+    results.innerHTML += `
+        <div class="book-card">
+
+            <img src="${imageUrl}"
+                 onerror="this.src='images/noimage.png'">
+
+            <h3>${book.title}</h3>
+
+            <p>ISBN : ${isbn}</p>
+
+            <p>
+                <a href="${book.link["@id"]}" target="_blank">
+                    CiNiiで見る
+                </a>
+            </p>
+
+        </div>
+    `;
+
+});
     } catch (error) {
 
         console.error(error);
-
         alert("取得できませんでした。");
+
     }
 
 }
