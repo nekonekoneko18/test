a342036b4ef35fba9078953a435574188ca9ce76
nekonekoneko18 <kyusakusaku1935@gmail.com>
2026-07-08 20:10:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
-document.getElementById("searchButton").addEventListener("click", () => {
-    alert("次のステップで検索機能を追加します。");
-});
+const button = document.getElementById("searchButton");
+
+button.addEventListener("click", searchBooks);
+
+async function searchBooks() {
+
+    const keyword = document.getElementById("keyword").value;
+
+    if (keyword === "") {
+        alert("検索語を入力してください。");
+        return;
+    }
+
+    const url =
+        "https://ci.nii.ac.jp/books/opensearch/search?q="
+        + encodeURIComponent(keyword)
+        + "&format=json";
+
+    console.log(url);
+
+    try {
+
+        const response = await fetch(url);
+
+        console.log(response);
+
+        const data = await response.json();
+
+        console.log(data);
+
+        alert("取得成功！");
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("取得できませんでした。");
+    }
+
+}
